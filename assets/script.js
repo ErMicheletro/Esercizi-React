dd31fd7646a5e290602aa8a6781d87919456432c
Michele Angelini <michele.angelini.031092@gmail.com>
2022-07-13 10:49:58 +0200

    modificheAnton

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
-const identità = {nome:"Mara", cognome:"Maionchi"};
+const identità = {nome:"Mara", cognome:"Maionchi", età:"81"};
 const musica = 'Crust punk';
 
 class Person extends React.Component {
     constructor(attributi){
-    super(attributi);
-    debugger
+        super(attributi);
+        this.state={peso:75}
+    }
+    cambiaPeso(event){
+        this.setState({peso:100})
+        event.preventDefault();
     }
     render(){
         return(
@@ -12,7 +16,13 @@ class Person extends React.Component {
             <h1>Anagrafica persona</h1>
             <h2>{this.props.anagrafica.nome}</h2>
             <h2>{this.props.anagrafica.cognome}</h2>
+            <h2>{this.props.anagrafica.età}</h2>
+            <h2>{this.state.peso}</h2>
             <h2> Ascolta {this.props.genere}</h2>
+            <form onSubmit={this.cambiaPeso.bind(this)}>
+                <input type="text" name="peso"/>
+                <button type="Submit">Invia</button>
+            </form>
         </div>
         )
     }
